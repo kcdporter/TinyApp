491ca0220419bf1279a3dbd66df581fe0babad3b
kcdporter <kiracdporter@gmail.com>
2019-03-12 21:20:29 +0000

    added post route for new url submissions

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true})); 
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -12,8 +14,6 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-
-
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
@@ -23,6 +23,20 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("WE DID IT")
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL,
+     longURL: urlDatabase[req.params.shortURL]};
+  res.render("urls_show", templateVars);
+});
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
