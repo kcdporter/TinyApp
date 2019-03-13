2f44869740b2efa97046470c518ede713429de19
kcdporter <kiracdporter@gmail.com>
2019-03-13 05:02:11 +0000

    build delete route in server that deletes url from database, and links MyUrl in /urls/:shortURL to /urls/

--- express_server.js
+++ express_server.js
@@ -1,6 +1,6 @@
-var express = require("express");
-var app = express();
-var PORT = 8080; // default port 8080
+const express = require("express");
+const app = express();
+const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true})); 
@@ -61,6 +61,13 @@ app.get("/urls/:shortURL", (req, res) => {
 
 app.get("/u/:shortURL", (req, res) => {
   res.redirect(`${urlDatabase[req.params.shortURL]}`);
+});
+
+app.post("/urls/:shortURL/delete", (req, res) => {
+  console.log(req.params);
+  delete urlDatabase[req.params.shortURL];
+  console.log(urlDatabase);
+  res.redirect("/urls");
 });
 
 app.get("/hello", (req, res) => {
