76f425fc2904f1e37143d0cb70373b2153c23b91
kcdporter <kiracdporter@gmail.com>
2019-03-16 00:17:11 +0000

    all of server.js has been linted

--- express_server.js
+++ express_server.js
@@ -41,11 +41,11 @@ const users = {
 function getRandomValue() {
   const chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
   let randomstring = '';
-	for (var i = 0; i < 6; i++) {
+	for (let i = 0; i < 6; i++) {
     const ranChar = Math.floor(Math.random() * chars.length);
     const newChars = chars.substring(ranChar,ranChar+1)
     randomstring += newChars;
-  }
+  };
   return randomstring;
 };
 
@@ -53,9 +53,9 @@ function authenticateUser(requestEmail, requestPassword){
   for(let key in users){
     if((users[key].email === requestEmail ) && bcrypt.compareSync(requestPassword, users[key].password)){
       return users[key];
-    }
-  }
-}
+    };
+  };
+};
 
 function urlsForUser(requestUser) {
   let urlList = [];
@@ -63,10 +63,10 @@ function urlsForUser(requestUser) {
     let urlId = urlDatabase[key];
     if (requestUser === urlId.user_id){
       urlList.push(urlDatabase[key]);
-    }
-  }
+    };
+  };
   return urlList;
-}
+};
 
 //Register && Login && Logout
 app.get('/register', (req, res) => {
@@ -74,7 +74,7 @@ app.get('/register', (req, res) => {
     users: users[req.session['user_id']]
   };
   res.render('registration', templateVars);
-})
+});
 
 app.post('/register', (req, res) => {
   const user_id = getRandomValue(req.body.email)
@@ -84,19 +84,19 @@ app.post('/register', (req, res) => {
       password: bcrypt.hashSync(req.body.password, 10)
     };
   if(!req.body.email || !req.body.password) {
-    res.status(400).send(`Please fill out both email and password fields`)
+    res.status(400).send(`Please fill out both email and password fields. Login <a href='/here'> Login</a>`)
   } else if (authenticateUser(newRegisteredUser.email) === true){
-      res.status(400).send(`Email is already registered, please <a href='/login'>Login</a>`)
+    res.status(400).send(`Email is already registered, please <a href='/login'>Login</a>`)
   } else {
     users[user_id] = newRegisteredUser;
     req.session.user_id = user_id;
     res.redirect('/urls');
-  }
+  };
 });
 
 app.get('/login', (req, res) => {
   res.render('login');
-})
+});
 
 app.post('/login', (req, res) => {
   let user = authenticateUser(req.body.email, req.body.password);
@@ -107,24 +107,24 @@ app.post('/login', (req, res) => {
     res.redirect('/urls');
   } else {
     res.status(403).send(`Email or password not recognized. If you haven't already, register <a href="/register"> here </a>, or try a different <a href="/login"> password. </a>`);
-    }
-})
+    };
+});
 
 app.post('/logout', (req, res) => {
   res.session = null;
   res.redirect('/login')
-})
+});
 
 //Reading URL Database 
 app.get('/urls', (req, res) => {
   if (!req.session.user_id){
     res.redirect('/login');
-  }
+  };
   let urlDb = urlsForUser(req.session.user_id);
   let templateVars = {
     urls: urlDb,
     users: users[req.session.user_id]
-};
+  };
   res.render('urls_index', templateVars);
 });
 
@@ -135,7 +135,7 @@ app.get('/urls/new', (req, res) => {
   } else {
   let templateVars = { 
     users: users[req.session.user_id],
-  }
+  };
   res.render('urls_new', templateVars);
   };
 });
@@ -143,13 +143,15 @@ app.get('/urls/new', (req, res) => {
 app.post('/urls', (req, res) => {
   const longURL = req.body.longURL;
   const shortURL = getRandomValue(longURL);
-  urlDatabase[shortURL] = { longURL, user_id: (req.session.user_id), shortURL};
+  urlDatabase[shortURL] = { 
+    longURL, 
+    user_id: (req.session.user_id), 
+    shortURL};
   let urlDb = urlsForUser(req.session.user_id);
   let templateVars = {
     urls: urlDb,
     users: users[req.session.user_id]
-};
-  // res.redirect(`/urls/${shortURL}`);
+  };
   res.render('urls_index', templateVars);
 });
 
@@ -157,7 +159,7 @@ app.post('/urls', (req, res) => {
 app.get('/urls/:shortURL', (req, res) => {
   if(!req.session.user_id|| !urlDatabase[req.params.shortURL] || urlDatabase[req.params.shortURL].user_id !== req.session.user_id){
     res.status(403).send(`This ShortURL belongs to a registered user. Please login to access. Please <a href="/login">login</a>`);
-  }
+  };
   let templateVars = { 
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL],
@@ -169,18 +171,22 @@ app.get('/urls/:shortURL', (req, res) => {
 //Edit && Delete URLs
 app.post('/urls/:shortURL', (req, res) => {
   if(!req.session.user_id|| !urlDatabase[req.params.shortURL] || urlDatabase[req.params.shortURL].user_id !== req.session.user_id){
-    res.status(403).send(`This ShortURL belongs to a registered user. Please login to access. Please <a href="/login">login</a>`);
-  }
+    res.status(403).send(`This ShortURL belongs to a registered user. Please login to access. Please <a href="/login">login</a>`)
+  };
   let shortURL = req.params.shortURL;
   let longURL = req.body.longURL;
-  urlDatabase[shortURL] = { longURL, user_id: (req.session.user_id), shortURL};
+  urlDatabase[shortURL] = { 
+    longURL, 
+    user_id: (req.session.user_id), 
+    shortURL
+  };
   res.redirect('/urls/');
-})
+});
 
 app.post('/urls/:shortURL/delete', (req, res) => {
   if(!req.session.user_id || urlDatabase[req.params.shortURL].user_id !== req.session.user_id){
     res.status(403).send(`This ShortURL belongs to a registered user. Please login to access. Please <a href="/login">login</a>`);
-  } 
+  }; 
   delete urlDatabase[req.params.shortURL];
   res.redirect('/urls');
 });
